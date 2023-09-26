a178eefc85d6869008b700ace3abccd3215ff084
kennyhalling <122585704+kennyhalling@users.noreply.github.com>
2023-09-26 13:35:31 -0500

    Combinator Activity

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,3 +1,4 @@
 let d = new Date();
 document.querySelector("#copyright").innerHTML = `&copy${d.getFullYear()}`;
 document.querySelector("#lastModified").innerHTML = `Last Modified: ${document.lastModified}`;
+document.querySelector("#name-and-country").innerHTML = `Kenneth S. Halling - United States`;
