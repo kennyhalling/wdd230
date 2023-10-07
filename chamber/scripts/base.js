cee8235a07f57c82f6c24c0ae72048f1193a5652
kennyhalling <122585704+kennyhalling@users.noreply.github.com>
2023-10-07 18:27:54 -0500

    CoC Home Page v1

---
+++ chamber/scripts/base.js
@@ -0,0 +1,9 @@
+document.querySelector("#last_modified").innerHTML = `Last Modified: ${document.lastModified}`;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
