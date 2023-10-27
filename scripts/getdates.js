02bcc7ca6ca56a191aaf472e7aa64404d67d0ce8
kennyhalling <122585704+kennyhalling@users.noreply.github.com>
2023-10-26 22:58:13 -0500

    Home Page Enhancement

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,3 +2,16 @@ let d = new Date();
 document.querySelector("#copyright").innerHTML = `&copy${d.getFullYear()}`;
 document.querySelector("#lastModified").innerHTML = `Last Modified: ${document.lastModified}`;
 document.querySelector("#name-and-country").innerHTML = `Kenneth S. Halling - United States`;
+
+const visitsDiplay = document.querySelector("#visits");
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+if  (numVisits == 0){
+    visitsDiplay.textContent = "Welcome to my webiste!"
+}
+else{
+    visitsDiplay.textContent = `Number of Website Visits: ${numVisits}`
+}
+
+numVisits++
+
+localStorage.setItem("numVisits-ls", numVisits);
